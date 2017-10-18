3b72d9e26e2b1009ee98a8ed5640a9bea8e245ef
hy-zhang <zhanghaoyuan00@gmail.com>
2017-10-18 13:39:31 +0800

    Impl.

--- Calculus/src/framework/FHJ.scala
+++ Calculus/src/framework/FHJ.scala
@@ -1,122 +1,11 @@
 package framework
 
-import scala.util.parsing.combinator._
-import scala.util.parsing.combinator.syntactical._
-
-object FHJ extends StandardTokenParsers with PackratParsers {
-  
-  case class Info(table: List[String], typeMap: Map[String, List[String]],
-      methodMap: Map[String, List[MethDef]], typeDefMap: Map[String, TypeDef]) {
-    def ext(t1: String, t2: String): Boolean = {
-      if (!table.contains(t1) || !table.contains(t2)) throw new Exception("Error: ext.")
-      typeMap.get(t1).contains(t2)
-    }
-    def subType(t1: String, t2: String): Boolean = {
-      if (!table.contains(t1) || !table.contains(t2)) throw new Exception("Error: subType.")
-      if (t1 == t2) return true
-      typeMap.get(t1).get.exists(x => subType(x, t2))
-    }
-    def dispatch(i: String, m: String, j: String): Option[MethDef] = {
-      if (!table.contains(i) || !table.contains(j)) throw new Exception("Error: dispatch.")
-      methodMap.get(i).get.find(x => x.name == m && x.update == j)
-    }
-    def prune(set: Set[String]): Set[String] = set.filter(i => !set.exists(j => j != i && subType(j, i)))
-    def collectMethods(i: String): Set[String] = {
-      if (!table.contains(i)) throw new Exception("Error: collectMethods.")
-      val start = methodMap.get(i).get.map(x => x.name).toSet
-      val op = (s: Set[String], x: String) => s ++ collectMethods(x)
-      typeMap.get(i).get.foldLeft(start)(op)
-    }
-    def mbody(m: String, id: String, is: String): Option[(String, List[(String, String)], (String, Option[Expr]))] = {
-      if (!table.contains(id) || !table.contains(is)) throw new Exception("Error: mbody.")
-      val set1 = mostSpecific(m, id, is)
-      if (set1.size != 1) return None
-      val i = set1.head
-      val set2 = mostSpecificOverride(m, id, i)
-      if (set2.size != 1) return None
-      val j = set2.head
-      val method = methodMap.get(j).get.find(x => x.name == m && x.update == i)
-      if (method.isEmpty) None else Some(j, method.get.paras, (method.get.returnType, method.get.returnExpr))
-    }
-    def mtype(m: String, i: String): Option[(List[String], String)] = {
-      if (!table.contains(i)) throw new Exception("Error: mtype.")
-      val body = mbody(m, i, i)
-      if (body.isEmpty) None else Some(body.get._2.map(p => p._1), body.get._3._1)
-    }
-    def mostSpecific(m: String, i: String, j: String): Set[String] = {
-      if (!table.contains(i) || !table.contains(j)) throw new Exception("Error: mostSpecific.")
-      val set = table.filter(k => subType(i, k) && (subType(k, j) || subType(j, k)) && dispatch(k, m, k).isDefined).toSet
-      prune(set)
-    }
-    def mostSpecificOverride(m: String, i: String, j: String): Set[String] = {
-      if (!table.contains(i) || !table.contains(j)) throw new Exception("Error: mostSpecificOverride.")
-      val set = table.filter(k => subType(k, j) && subType(i, k) && dispatch(k, m, j).isDefined).toSet
-      prune(set)
-    }
-    def canOverride(m: String, i: String, j: String): Boolean = {
-      if (!table.contains(i) || !table.contains(j)) throw new Exception("Error: canOverride.")
-      val meth = dispatch(i, m, i)
-      val meth2 = dispatch(j, m, j)
-      if (meth.isEmpty || meth2.isEmpty) return false
-      meth.get.returnType == meth2.get.returnType && meth.get.paras.zip(meth2.get.paras).forall(p => p._1._1 == p._2._1)
-    }
-    def canInstantiate(i: String): Boolean = {
-      if (!table.contains(i)) throw new Exception("Error: canInstantiate.")
-      val methods = collectMethods(i)
-      methods.forall(m => mostSpecific(m, i, i).forall(j => {
-        val set = mostSpecificOverride(m, i, j)
-        set.size == 1 && dispatch(set.head, m, j).exists(meth => meth.returnExpr.isDefined)
-      }))
-    }
-  }
-  
+object FHJ {
+  /* Environment. */
   type Env = {val info: Info; val gamma: Map[String, String]}
   
-  case class Program(is: List[TypeDef], e: Expr) {
-    val collectInfo: Option[Info] = {
-      var error: Boolean = false
-      val table = is.map(x => x.name)
-      error = table.distinct.size != table.size
-      if (error) None else {
-        val typeMap = is.map(x => {
-          if (x.sups.distinct.size != x.sups.size) error = true
-          if (x.sups.exists(y => !table.contains(y))) error = true
-          x.name -> x.sups}).toMap
-        if (error) None else {
-          val methodMap = is.map(x => {
-            val methods = x.methods.map(y => (y.name, y.update))
-            if (methods.distinct.size != methods.size) error = true
-            x.name -> x.methods}).toMap
-          if (error) None else Some(Info(table, typeMap, methodMap, is.map(x => x.name -> x).toMap))
-        }
-      }
-    }
-    def interfaceCheck: ((Env, Boolean, Set[String]), String) => (Env, Boolean, Set[String]) = (tuple, i) => {
-      val (env, res, checked) = tuple
-      if (!res || checked.contains(i)) (env, res, checked) else {
-        val newTuple = env.info.typeMap.get(i).get.foldLeft(tuple)(interfaceCheck)
-        val checkSelf = env.info.typeDefMap.get(i).get.interfaceCheck(env)
-        (env, newTuple._2 && checkSelf, newTuple._3 + i)
-      }
-    }
-    def programCheck: Either[String, String] = {
-      if (collectInfo.isEmpty) return Left("Incorrect Info")
-      val env = new {val info = collectInfo.get; val gamma = Map[String, String]()}
-      if (!env.info.table.foldLeft((env, true, Set[String]()))(interfaceCheck)._2) return Left("Interface check failed")
-      val checkType = e.checkType(env)
-      if (checkType.isEmpty) Left("Expression type-check failed") else Right(checkType.get)
-    }
-    def run: Either[String, Value] = {
-      val program_check = programCheck
-      if (program_check.isLeft) Left(program_check.left.get)
-      else Right(e.eval(new {val info = collectInfo.get; val gamma = Map[String, String]()}))
-    }
-    override def toString = {
-      val isStr = if (is.isEmpty) "" else is.map(x => x.toString).reduce((a, b) => a + "\n" + b) + "\n\n"
-      isStr + e.toString
-    }
-  }
-  
+  /* Abstract syntax tree. */
+  /* Typing and semantic rules. */
   case class TypeDef(name: String, sups: List[String], methods: List[MethDef]) {
     def interfaceCheck(env: Env): Boolean = {
       if (sups.exists(x => !env.info.table.contains(x))) return false
@@ -235,123 +124,114 @@ object FHJ extends StandardTokenParsers with PackratParsers {
     override def toString = "<" + is + "> new " + id + "()"
   }
   
-  lexical.reserved += ("interface", "extends", "return", "new", "override", "super")
-  lexical.delimiters += ("(", ")", "{", "}", ",", ".", "::", ";")
-  
-  val lcid = ident ^? { case id if id.charAt(0).isLower => id }
-  val ucid = ident ^? { case id if id.charAt(0).isUpper => id }
-  
-  object Parser {
-    val pP : PackratParser[Program] = rep(pTD) ~ pE ^^ { case l ~ e => new Program(l, e) }
-    val pTD : PackratParser[TypeDef] =
-      ("interface" ~> ucid) ~ ("extends" ~> repsep(ucid, ",") | success(List())) ~ ("{" ~> rep(pMD) <~ "}") ^^
-        { case n ~ sups ~ ms => new TypeDef(n, sups, ms) }
-    val pMD : PackratParser[MethDef] =
-      ucid ~ lcid ~ ("(" ~> repsep(ucid ~ lcid ^^ { case t ~ n => (t, n) }, ",") <~ ")") ~
-        ("override" ~> ucid) ~ ("{" ~> ("return" ~> pE <~ ";") <~ "}" ^^ { case pE => Some(pE) } | ";" ^^^ None) ^^
-          { case t ~ m ~ paras ~ update ~ e => new MethDef(t, m, paras, update, e) }
-    val pE : PackratParser[Expr] =
-      pE ~ ("." ~> lcid) ~ ("(" ~> repsep(pE, ",") <~ ")") ^^
-        { case e ~ m ~ args => Invk(e, m, args) } |||
-      lcid ~ ("(" ~> repsep(pE, ",") <~ ")") ^^
-        { case m ~ args => Invk(Var("this"), m, args) } |||
-      ("(" ~> ucid <~ ")") ~ pE ^^ { case i ~ e => AnnoExpr(i, e) } |||
-      "new" ~> ucid <~ ("(" ~ ")") ^^ New ||| lcid ^^ Var ||| "(" ~> pE <~ ")" 
-  }
-  
-  def parse(in: String): Either[String, Program] = phrase(Parser.pP)(new lexical.Scanner(in)) match {
-      case t if t.successful => Right(t.get)
-      case t                 => Left(t.toString)
-  }
-  
-  def run(p: String) = {
-    val parsed = parse(p)
-    if (parsed.isLeft) println(parsed.left.get)
-    else {
-      val pretty = parsed.right.get.toString
-      println(pretty + "\n")
-      val eval = parsed.right.get.run
-      if (eval.isLeft) println("Type-check: " + eval.left.get)
-      else println("==> " + eval.right.get.toString)
+  /* Type-checker. */
+  case class Info(table: List[String], typeMap: Map[String, List[String]],
+      methodMap: Map[String, List[MethDef]], typeDefMap: Map[String, TypeDef]) {
+    def ext(t1: String, t2: String): Boolean = {
+      if (!table.contains(t1) || !table.contains(t2)) throw new Exception("Error: ext.")
+      typeMap.get(t1).contains(t2)
+    }
+    def subType(t1: String, t2: String): Boolean = {
+      if (!table.contains(t1) || !table.contains(t2)) throw new Exception("Error: subType.")
+      if (t1 == t2) return true
+      typeMap.get(t1).get.exists(x => subType(x, t2))
+    }
+    def dispatch(i: String, m: String, j: String): Option[MethDef] = {
+      if (!table.contains(i) || !table.contains(j)) throw new Exception("Error: dispatch.")
+      methodMap.get(i).get.find(x => x.name == m && x.update == j)
+    }
+    def prune(set: Set[String]): Set[String] = set.filter(i => !set.exists(j => j != i && subType(j, i)))
+    def collectMethods(i: String): Set[String] = {
+      if (!table.contains(i)) throw new Exception("Error: collectMethods.")
+      val start = methodMap.get(i).get.map(x => x.name).toSet
+      val op = (s: Set[String], x: String) => s ++ collectMethods(x)
+      typeMap.get(i).get.foldLeft(start)(op)
+    }
+    def mbody(m: String, id: String, is: String): Option[(String, List[(String, String)], (String, Option[Expr]))] = {
+      if (!table.contains(id) || !table.contains(is)) throw new Exception("Error: mbody.")
+      val set1 = mostSpecific(m, id, is)
+      if (set1.size != 1) return None
+      val i = set1.head
+      val set2 = mostSpecificOverride(m, id, i)
+      if (set2.size != 1) return None
+      val j = set2.head
+      val method = methodMap.get(j).get.find(x => x.name == m && x.update == i)
+      if (method.isEmpty) None else Some(j, method.get.paras, (method.get.returnType, method.get.returnExpr))
+    }
+    def mtype(m: String, i: String): Option[(List[String], String)] = {
+      if (!table.contains(i)) throw new Exception("Error: mtype.")
+      val body = mbody(m, i, i)
+      if (body.isEmpty) None else Some(body.get._2.map(p => p._1), body.get._3._1)
+    }
+    def mostSpecific(m: String, i: String, j: String): Set[String] = {
+      if (!table.contains(i) || !table.contains(j)) throw new Exception("Error: mostSpecific.")
+      val set = table.filter(k => subType(i, k) && (subType(k, j) || subType(j, k)) && dispatch(k, m, k).isDefined).toSet
+      prune(set)
+    }
+    def mostSpecificOverride(m: String, i: String, j: String): Set[String] = {
+      if (!table.contains(i) || !table.contains(j)) throw new Exception("Error: mostSpecificOverride.")
+      val set = table.filter(k => subType(k, j) && subType(i, k) && dispatch(k, m, j).isDefined).toSet
+      prune(set)
+    }
+    def canOverride(m: String, i: String, j: String): Boolean = {
+      if (!table.contains(i) || !table.contains(j)) throw new Exception("Error: canOverride.")
+      val meth = dispatch(i, m, i)
+      val meth2 = dispatch(j, m, j)
+      if (meth.isEmpty || meth2.isEmpty) return false
+      meth.get.returnType == meth2.get.returnType && meth.get.paras.zip(meth2.get.paras).forall(p => p._1._1 == p._2._1)
+    }
+    def canInstantiate(i: String): Boolean = {
+      if (!table.contains(i)) throw new Exception("Error: canInstantiate.")
+      val methods = collectMethods(i)
+      methods.forall(m => mostSpecific(m, i, i).forall(j => {
+        val set = mostSpecificOverride(m, i, j)
+        set.size == 1 && dispatch(set.head, m, j).exists(meth => meth.returnExpr.isDefined)
+      }))
     }
   }
   
-  val program1 = "interface A { A m() override A { return new A(); } }" +
-                 "interface B { B m() override B { return new B(); } }" +
-                 "interface C extends A, B {}" +
-                 "((A) new C()).m()"
-                 
-  val program2 = "interface A { A m() override A { return new A(); } }" +
-                 "interface B { A m() override B { return new A(); } }" +
-                 "interface C extends A, B { A m() override C { return new C(); } }" +
-                 "((A) new C()).m()"
-                 
-  val program3 = "interface A { A m() override A { return new A(); } }" +
-                 "interface B { B m() override B { return new B(); } }" +
-                 "interface C extends A, B { A m() override A { return new C(); } }" +
-                 "((A) new C()).m()"
-                 
-  val program4 = "interface T { T m() override T { return new T(); } }" + 
-                 "interface A extends T { T m() override A { return new A(); } }" +
-                 "interface B extends T { T m() override B { return new B(); } }" +
-                 "interface C extends A, B {}" +
-                 "((T) new C()).m()"
-                 
-  val program5 = "interface T { T m() override T { return new T(); } }" + 
-                 "interface A extends T { T m() override T { return new A(); } }" +
-                 "interface B extends T { T m() override T { return new B(); } }" +
-                 "interface C extends A, B {}" +
-                 "((T) new C()).m()"
-                 
-  val program6 = "interface T { T m() override T { return new T(); } }" + 
-                 "interface A extends T { T m() override A { return new A(); } }" +
-                 "interface B extends T { T m() override B { return new B(); } }" +
-                 "interface C extends A, B { T m() override C { return new C(); } }" +
-                 "((T) new C()).m()"
-                 
-  val program7 = "interface P {}" +
-                 "interface FromDeck extends P {}" +
-                 "interface FromSafeDeck extends P {}" +
-                 "interface FromDrawable extends P {}" +
-                 "interface FromDrawableSafeDeck extends P {}" +                
-                 "interface Deck { P draw() override Deck {return new FromDeck();}" +
-                                 " P shuffleAndDraw() override Deck {return draw();}}" +
-                 "interface SafeDeck extends Deck { P draw() override SafeDeck {return new FromSafeDeck();}}" +
-                 "interface Drawable { P draw() override Drawable {return new FromDrawable();}}" +
-                 "interface DrawableSafeDeck extends Drawable, SafeDeck {" +
-                 "P draw() override Drawable {return new FromDrawableSafeDeck();}}" +
-                 "new DrawableSafeDeck().shuffleAndDraw()"
-                 
-  val program8 = "interface P {}" +
-                 "interface FromDeck extends P {}" +
-                 "interface FromSafeDeck extends P {}" +
-                 "interface FromDrawable extends P {}" +
-                 "interface FromDrawableSafeDeck extends P {}" +                
-                 "interface Deck { P draw() override Deck {return new FromDeck();}" +
-                                 " P shuffleAndDraw() override Deck {return draw();}}" +
-                 "interface SafeDeck extends Deck { P draw() override SafeDeck {return new FromSafeDeck();}}" +
-                 "interface Drawable { P draw() override Drawable {return new FromDrawable();}}" +
-                 "interface DrawableSafeDeck extends Drawable, SafeDeck {" +
-                 "P draw() override Drawable {return new FromDrawableSafeDeck();}}" +
-                 "((Drawable) new DrawableSafeDeck()).draw()"                 
-  
-  def main(args : Array[String]) = {
-    println("----------------------------------")
-    run(program1)
-    println("----------------------------------")
-    run(program2)
-    println("----------------------------------")
-    run(program3)
-    println("----------------------------------")
-    run(program4)
-    println("----------------------------------")
-    run(program5)
-    println("----------------------------------")
-    run(program6)
-    println("----------------------------------")
-    run(program7)
-    println("----------------------------------")
-    run(program8)
-    println("----------------------------------")
+  case class Program(is: List[TypeDef], e: Expr) {
+    val collectInfo: Option[Info] = {
+      var error: Boolean = false
+      val table = is.map(x => x.name)
+      error = table.distinct.size != table.size
+      if (error) None else {
+        val typeMap = is.map(x => {
+          if (x.sups.distinct.size != x.sups.size) error = true
+          if (x.sups.exists(y => !table.contains(y))) error = true
+          x.name -> x.sups}).toMap
+        if (error) None else {
+          val methodMap = is.map(x => {
+            val methods = x.methods.map(y => (y.name, y.update))
+            if (methods.distinct.size != methods.size) error = true
+            x.name -> x.methods}).toMap
+          if (error) None else Some(Info(table, typeMap, methodMap, is.map(x => x.name -> x).toMap))
+        }
+      }
+    }
+    def interfaceCheck: ((Env, Boolean, Set[String]), String) => (Env, Boolean, Set[String]) = (tuple, i) => {
+      val (env, res, checked) = tuple
+      if (!res || checked.contains(i)) (env, res, checked) else {
+        val newTuple = env.info.typeMap.get(i).get.foldLeft(tuple)(interfaceCheck)
+        val checkSelf = env.info.typeDefMap.get(i).get.interfaceCheck(env)
+        (env, newTuple._2 && checkSelf, newTuple._3 + i)
+      }
+    }
+    def programCheck: Either[String, String] = {
+      if (collectInfo.isEmpty) return Left("Incorrect Info")
+      val env = new {val info = collectInfo.get; val gamma = Map[String, String]()}
+      if (!env.info.table.foldLeft((env, true, Set[String]()))(interfaceCheck)._2) return Left("Interface check failed")
+      val checkType = e.checkType(env)
+      if (checkType.isEmpty) Left("Expression type-check failed") else Right(checkType.get)
+    }
+    def run: Either[String, Value] = {
+      val program_check = programCheck
+      if (program_check.isLeft) Left(program_check.left.get)
+      else Right(e.eval(new {val info = collectInfo.get; val gamma = Map[String, String]()}))
+    }
+    override def toString = {
+      val isStr = if (is.isEmpty) "" else is.map(x => x.toString).reduce((a, b) => a + "\n" + b) + "\n\n"
+      isStr + e.toString
+    }
   }
 }
---
+++ Calculus/src/framework/Main.scala
@@ -0,0 +1,41 @@
+package framework
+
+import scala.io.Source
+
+object Main {
+  def run(fileName: String): Unit = {
+    val program = Source.fromFile("examples/" + fileName + ".fhj").mkString
+    val parsed = FHJParser.parse(program)
+    if (parsed.isLeft) println(parsed.left.get)
+    else {
+      val pretty = parsed.right.get.toString
+      println(pretty + "\n")
+      val eval = parsed.right.get.run
+      if (eval.isLeft) println("Type-check: " + eval.left.get)
+      else println("==> " + eval.right.get.toString)
+    }
+  }               
+  
+  def main(args : Array[String]) = {
+    println("----------------------------------\nExample (a):\n")
+    run("Example_a")
+    println("----------------------------------\nExample (b):\n")
+    run("Example_b")
+    println("----------------------------------\nExample (c):\n")
+    run("Example_c")
+    println("----------------------------------\nExample (d):\n")
+    run("Example_d")
+    println("----------------------------------\nExample (e):\n")
+    run("Example_e")
+    println("----------------------------------\nExample (f):\n")
+    run("Example_f")
+    println("----------------------------------\nExample (Problem 1):\n")
+    run("Example_p1")
+    println("----------------------------------\nExample (Problem 2):\n")
+    run("Example_p2")
+    println("----------------------------------\nExample (Problem 3):\n")
+    run("Example_p3")
+    println("----------------------------------")
+  }
+  
+}
