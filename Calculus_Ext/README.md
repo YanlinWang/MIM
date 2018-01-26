90e5aa878f60c4d2a1014df05a228ed31f333913
hy-zhang <zhanghaoyuan00@gmail.com>
2018-01-26 17:40:28 +0800

    Ext impl.

---
+++ Calculus_Ext/README.md
@@ -0,0 +1,13 @@
+# FHJ: A Formal Model for Hierarchical Dispatching and Overriding
+
+This folder contains a Scala project that implements FHJ as a simple interpreter.
+
+- [./src/framework/FHJ.scala](https://github.com/YanlinWang/MIM/blob/master/Calculus/src/framework/FHJ.scala): the abstract syntax of FHJ, typing and semantic rules.
+- [./src/framework/Parser.scala](https://github.com/YanlinWang/MIM/blob/master/Calculus/src/framework/Parser.scala): the parser implemented by Scala Packrat Parsing.
+- [./src/framework/Main.scala](https://github.com/YanlinWang/MIM/blob/master/Calculus/src/framework/Main.scala): the main program to run the examples.
+
+## How to run
+The project is developed using Scala Eclipse IDE on Windows. Please install Scala Eclipse, go back to the [parent directory](https://github.com/YanlinWang/MIM) and import the entire fold "Calculus/" into the IDE as a Scala project. Run __Main.scala__ as a Scala application.
+
+## Examples
+[./examples/](https://github.com/YanlinWang/MIM/tree/master/Calculus/examples) has some examples introduced in the paper. You can also write your own programs and run them in Main.
---
+++ Calculus_Ext/src/framework/Parser.scala
@@ -0,0 +1,37 @@
+package framework
+
+import scala.util.parsing.combinator._
+import scala.util.parsing.combinator.syntactical._
+
+object FHJParser extends StandardTokenParsers with PackratParsers {
+  import FHJ._
+  
+  lexical.reserved += ("interface", "extends", "return", "new", "override", "super")
+  lexical.delimiters += ("(", ")", "{", "}", ",", ".", "::", ";")
+  
+  val lcid = ident ^? { case id if id.charAt(0).isLower => id }
+  val ucid = ident ^? { case id if id.charAt(0).isUpper => id }
+  
+  object Parser {
+    val pP : PackratParser[Program] = rep(pTD) ~ pE ^^ { case l ~ e => new Program(l, e) }
+    val pTD : PackratParser[TypeDef] =
+      ("interface" ~> ucid) ~ ("extends" ~> repsep(ucid, ",") | success(List())) ~ ("{" ~> rep(pMD) <~ "}") ^^
+        { case n ~ sups ~ ms => new TypeDef(n, sups, ms) }
+    val pMD : PackratParser[MethDef] =
+      ucid ~ lcid ~ ("(" ~> repsep(ucid ~ lcid ^^ { case t ~ n => (t, n) }, ",") <~ ")") ~
+        ("override" ~> rep1sep(ucid, ",")) ~ ("{" ~> ("return" ~> pE <~ ";") <~ "}" ^^ { case pE => Some(pE) } | ";" ^^^ None) ^^
+          { case t ~ m ~ paras ~ update ~ e => new MethDef(t, m, paras, update, e) }
+    val pE : PackratParser[Expr] =
+      pE ~ ("." ~> lcid) ~ ("(" ~> repsep(pE, ",") <~ ")") ^^
+        { case e ~ m ~ args => Invk(e, m, args) } |||
+      lcid ~ ("(" ~> repsep(pE, ",") <~ ")") ^^
+        { case m ~ args => Invk(Var("this"), m, args) } |||
+      ("(" ~> ucid <~ ")") ~ pE ^^ { case i ~ e => AnnoExpr(i, e) } |||
+      "new" ~> ucid <~ ("(" ~ ")") ^^ New ||| lcid ^^ Var ||| "(" ~> pE <~ ")" 
+  }
+  
+  def parse(in: String): Either[String, Program] = phrase(Parser.pP)(new lexical.Scanner(in)) match {
+      case t if t.successful => Right(t.get)
+      case t                 => Left(t.toString)
+  }
+}
